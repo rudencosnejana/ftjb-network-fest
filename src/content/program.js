7eef168920dfa171dbb1228154ae80928eec6fad
Yehoslav Rudenco <yehoslavrudenco@protonmail.com>
2026-04-09 13:34:43 +0300

    Tombola and UI improvements
    - Fixed some spacings
    - Improved responsivines on a wider variety of screens

--- src/content/program.js
+++ src/content/program.js
@@ -1,19 +1,22 @@
 export default [
     {
-        timp: "09:00-10:00",
-        titlu: "Etapa 1: Amenajarea standurilor",
-        descr:
-            "Participanții sunt invitați să își amenajeze standurile. Fiecare organizație este responsabilă de organizarea propriului stand, inclusiv de aducerea materialelor promoționale.",
-    },
-    {
         timp: "11:00-16:00",
-        titlu: "Etapa 2: Desfășurarea târgului ONG-urilor",
+        titlu: "Desfășurarea târgului ONG-urilor",
+        public: true,
         descr:
             "Vizitatorii vor explora standurile, unde fiecare organizație participantă își va promova activitatea prin materiale promoționale, oportunități de voluntariat sau alte inițiative pe care dorește să le aducă în atenția publicului.",
     },
     {
+        timp: "???",
+        titlu: "Extragera cîsțigătorului",
+        public: true,
+        descr:
+            "Extragerea câștigătorilor va avea loc la finalul evenimentului, iar participanții vor putea afla rezultatele urmărind pagina Federației Tinerilor din Județul Brașov",
+    },
+    {
         timp: "18:30",
-        titlu: "Etapa 3: Seara socială",
+        titlu: "Seara socială",
+        public: false,
         descr:
             "<b>Organizațiile participante</b> sunt invitate la o seară socială organizată la Zbor Hub Brașov, într-un cadru informal de networking și cunoaștere între organizații",
     },
